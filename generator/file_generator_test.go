50283b8a6548b7c8e18846735f8becb9cc68a40b
Dylan.Wen <hhkbp2@gmail.com>
2016-03-04 10:36:56 +0800

    add test for file generator

---
+++ generator/file_generator_test.go
@@ -0,0 +1,21 @@
+package generator
+
+import (
+	"fmt"
+	"github.com/hhkbp2/testify/require"
+	"testing"
+)
+
+func TestFileGenerator(t *testing.T) {
+	filename := "file_generator.data"
+	var g Generator
+	fg, err := NewFileGenerator(filename)
+	require.Nil(t, err)
+	g = fg
+	total := 5
+	for i := 1; i < total; i++ {
+		last := g.NextString()
+		require.Equal(t, last, fmt.Sprintf("%d", i))
+		require.Equal(t, last, g.LastString())
+	}
+}
