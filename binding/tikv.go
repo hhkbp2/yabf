a58aa59a6e73be5ab673613ae24265e9c61fb62d
Dylan.Wen <hhkbp2@gmail.com>
2016-12-21 11:30:49 +0800

    make tikv client singleton

--- binding/tikv.go
+++ binding/tikv.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hhkbp2/yabf"
 	"github.com/pingcap/tidb/store/tikv"
 	"strings"
+	"sync"
 )
 
 const (
@@ -11,10 +12,14 @@ const (
 	PropertyTikvPDAddressDefault = "127.0.0.1:2379"
 )
 
+var (
+	pdAddress  string
+	tikvClient *tikv.RawKVClient
+	once       sync.Once
+)
+
 type TikvDB struct {
 	*yabf.DBBase
-	pdAddress string
-	client    *tikv.RawKVClient
 }
 
 func NewTikvDB() *TikvDB {
@@ -24,14 +29,15 @@ func NewTikvDB() *TikvDB {
 }
 
 func (self *TikvDB) Init() error {
-	props := self.GetProperties()
-	pdAddress := props.GetDefault(PropertyTikvPDAddress, PropertyTikvPDAddressDefault)
-	client, err := tikv.NewRawKVClient(strings.Split(pdAddress, ","))
-	if err != nil {
-		return err
-	}
-	self.pdAddress = pdAddress
-	self.client = client
+	once.Do(func() {
+		props := self.GetProperties()
+		pdAddress := props.GetDefault(PropertyTikvPDAddress, PropertyTikvPDAddressDefault)
+		var err error
+		tikvClient, err = tikv.NewRawKVClient(strings.Split(pdAddress, ","))
+		if err != nil {
+			panic("fail to init tikv RawKVClient")
+		}
+	})
 	return nil
 }
 
@@ -45,7 +51,7 @@ func (self *TikvDB) Read(table string, key string, fields []string) (yabf.KVMap,
 		return nil, yabf.StatusBadRequest
 	}
 
-	v, err := self.client.Get([]byte(key))
+	v, err := tikvClient.Get([]byte(key))
 	if err != nil {
 		return nil, yabf.StatusError
 	}
@@ -71,14 +77,14 @@ func (self *TikvDB) Insert(table string, key string, values yabf.KVMap) yabf.Sta
 	for _, v := range values {
 		value = v
 	}
-	if err := self.client.Put([]byte(key), value); err != nil {
+	if err := tikvClient.Put([]byte(key), value); err != nil {
 		return yabf.StatusError
 	}
 	return yabf.StatusOK
 }
 
 func (self *TikvDB) Delete(table string, key string) yabf.StatusType {
-	if err := self.client.Delete([]byte(key)); err != nil {
+	if err := tikvClient.Delete([]byte(key)); err != nil {
 		return yabf.StatusError
 	}
 	return yabf.StatusOK
